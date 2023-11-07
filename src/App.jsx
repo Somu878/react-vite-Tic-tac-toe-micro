c5403d3ec4e89121d17c64c04b9f2d53cba23cc0
somu878 <somumydhili19@gmail.com>
2023-11-07 17:00:27 +0530

    initial commit -added quotes container

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+import Quote from './quotes/quote'
+function App() {
+
+  return (
+    <>
+      hello all
+      <Quote/>
+    </>
+  )
+}
+
+export default App
---
+++ src/quotes/Quote.jsx
@@ -0,0 +1,48 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import './quote.css'
+
+function Quote() {
+    const [quoteNumber, setQuoteNumber] = useState(1);
+    const [quote, setQuote] = useState('Please Wait');
+
+    useEffect(() => {
+        const fetchQuoteDelay = () => {
+                fetchQuote();
+        };
+        fetchQuoteDelay();
+        const interval = setInterval(() => {
+            setQuoteNumber(prevNumber => prevNumber + 1);
+            fetchQuoteDelay();
+        }, 60000);
+        return () => {
+            clearInterval(interval);
+        };
+    }, []);
+
+    function fetchQuote() {
+        let url = `https://api.adviceslip.com/advice`;
+        axios.get(url)
+            .then((res) => {
+                setQuote(res.data.slip.advice);
+                if (advice && advice.split(' ').length <= 8) {
+                    setQuote(advice);
+                } else {
+                    fetchQuote();
+                }
+            })
+            .catch((err) => {
+                console.log(err);
+            });
+    }
+
+    return (
+        <div className='quotecomponent'>
+            <div className='q'>Quote #{quoteNumber}</div>
+            <div className='quote'>"{quote}"</div>
+            <div className='ecllipse'></div>
+        </div>
+    );
+}
+
+export default Quote;
